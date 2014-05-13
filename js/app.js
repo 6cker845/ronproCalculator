fd5312653aa0501b2875ec405b4d38ba6061abd7
6cker845 <nick_fun6king@yahoo.co.jp>
2014-05-13 12:03:44 +0900

    計算機システム完成

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 var inputoperandA = document.querySelector("#operand_a");
-var inputoperandB = document.querySelector("#operand_b");
 var inputOperator = document.querySelector("#operator");
 var output = document.querySelector("#output");
 var error = document.querySelector("#error");
@@ -16,98 +15,56 @@ var showResult = function(result){
 	output.value = result + "";
 };
 
-var add = function(a, b){
-	return a + b;
-};
-
-var subtract = function(a, b){
-	return a - b;
-};
-
 var multiply = function(a, b){
 	return a * b;
 };
 
-var divide = function(a, b){
-	return a / b;
-};
-
-var modulus = function(a, b){
-	return a % b;
-};
-
 var isOperator = function(operator){
-	return operator == "+" || operator == "-" || operator == "*" ||
-	operator == "/" || operator == "%";
+	return operator == "コーラ" || operator == "スプライト" || operator == "オレンジジュース" ||
+	operator == "ビール" || operator == "ココア";
 };
 
 var isNumber = function(a){
 	return !Number.isNaN(a);
+	//全ての数値
 };
 
-var isNonZeroNumber = function(a){
-	return isNumber(a) && a != 0;
-};
-
-var isInteger = function(a){
-	return Number.isInteger(a);
-};
-
-var isNaturalNumber = function(a){
-	return isInteger(a) && a > 0;
-};
-
-var isDivision = function(operator, a, b){
-	return operator == "/" && isNumber(a) && isNonZeroNumber(b);
-};
-
-var isModulus = function(operator, a, b){
-	return operator == "%" && isNaturalNumber(a) && isNaturalNumber(b);
-};
-
-var isMultiplication = function(operator, a, b){
-	return operator == "*" && isNumber(a) && isNumber(b);
-};
-
-var isSubtraction = function(operator, a, b){
-	return operator == "-" && isNumber(a) && isNumber(b);
-};
-
-var isAddition = function(operator, a, b){
-	return operator == "+" && isNumber(a) && isNumber(b);
+var isMultiplication = function(operator, a){
+	return operator == "コーラ" && isNumber(a)|| operator == "スプライト" && isNumber(a)|| operator == "オレンジジュース" && isNumber(a) || operator == "ビール" && isNumber(a) || operator == "ココア" && isNumber(a);
 }
 
 var isReady = function(operator, a, b){
-	return isDivision(operator, a, b) || isModulus(operator, a, b) ||
-	isMultiplication(operator, a, b) || isSubtraction(operator, a, b) || isAddition(operator, a, b);
+	return isMultiplication(operator, a, b);
 };
 
 var startCalc = function(){
 	var operandA = inputoperandA.value;
-	var operandB = inputoperandB.value;
 	var operator = inputOperator.value;
-
+		
 	operandA = Number(operandA);
-	operandB = Number(operandB);
 
 	hideError();
-	if(isReady(operator, operandA, operandB)){
+	if(isReady(operator, operandA)){
 		var result = 0;
-		if(operator == "+"){
-			result = add(operandA, operandB);
-		}else if(operator == "-"){
-			result = subtract(operandA, operandB)
-		}else if(operator == "*"){
-			result = multiply(operandA, operandB);
-		}else if(operator == "/"){
-			result = divide(operandA, operandB);
-		}else if(operator == "%"){
-			result = modulus(operandA, operandB);
+		if(operator == "コーラ"){
+			result = multiply(225, operandA);
+		} else if (operator == "スプライト"){
+			result = multiply(205, operandA);
+		} else if (operator == "オレンジジュース"){
+			result = multiply(214, operandA);
+		} else if (operator == "ビール"){
+			result = multiply(200, operandA);
+		} else if (operator == "ココア"){
+			result = multiply(2060, operandA);
 		}
 		showResult(result);
 	}else{
 		showError();
 	}
+	
+	console.log(operator);
+	console.log(output);
+
 };
 
 var initApp = function(){
